84a2e662a223432a9e744315a4b90f60138e215f
sanchez <145916188+ravzasanchez@users.noreply.github.com>
2023-11-20 11:01:36 +0300

    Add files via upload

---
+++ form29/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace form29
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbSayilar = new System.Windows.Forms.ListBox();
+            this.btnAktar = new System.Windows.Forms.Button();
+            this.txtAranan = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lbSayilar
+            // 
+            this.lbSayilar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.lbSayilar.Font = new System.Drawing.Font("Perpetua Titling MT", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbSayilar.ForeColor = System.Drawing.SystemColors.Menu;
+            this.lbSayilar.FormattingEnabled = true;
+            this.lbSayilar.ItemHeight = 19;
+            this.lbSayilar.Location = new System.Drawing.Point(48, 12);
+            this.lbSayilar.Name = "lbSayilar";
+            this.lbSayilar.Size = new System.Drawing.Size(154, 194);
+            this.lbSayilar.TabIndex = 0;
+            // 
+            // btnAktar
+            // 
+            this.btnAktar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.btnAktar.Font = new System.Drawing.Font("Perpetua Titling MT", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAktar.ForeColor = System.Drawing.SystemColors.Menu;
+            this.btnAktar.Location = new System.Drawing.Point(48, 231);
+            this.btnAktar.Name = "btnAktar";
+            this.btnAktar.Size = new System.Drawing.Size(154, 47);
+            this.btnAktar.TabIndex = 1;
+            this.btnAktar.Text = "Hesapla";
+            this.btnAktar.UseVisualStyleBackColor = false;
+            this.btnAktar.Click += new System.EventHandler(this.btnAktar_Click);
+            // 
+            // txtAranan
+            // 
+            this.txtAranan.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.txtAranan.Font = new System.Drawing.Font("Perpetua Titling MT", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAranan.Location = new System.Drawing.Point(48, 302);
+            this.txtAranan.Multiline = true;
+            this.txtAranan.Name = "txtAranan";
+            this.txtAranan.Size = new System.Drawing.Size(154, 36);
+            this.txtAranan.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(293, 419);
+            this.Controls.Add(this.txtAranan);
+            this.Controls.Add(this.btnAktar);
+            this.Controls.Add(this.lbSayilar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lbSayilar;
+        private System.Windows.Forms.Button btnAktar;
+        private System.Windows.Forms.TextBox txtAranan;
+    }
+}
+
